4e6f38cf1c17aedc9a7e4a48e850084d8549f7e5
Alexander Marini <svenskost6@gmail.com>
2023-04-18 22:42:52 +0200

    form animation done

--- ticket.js
+++ ticket.js
@@ -1,5 +1,28 @@
-var submit = document.getElementById("submit")
+function SubmitFunc () {
+    
+}
 
+var namee = document.getElementById("name")
+var email = document.getElementById("email")
+var topics = document.getElementById("topics")
+var message = document.getElementById("message")
+var submit = document.getElementById("submit")
+var checkmark = document.getElementById("checkmark")
+var submittedText = document.getElementById("submittedText")
+function SubmitAnim () {
+    namee.style.opacity = 0
+    email.style.opacity = 0
+    topics.style.opacity = 0
+    message.style.opacity = 0
+    submit.style.opacity = 0
+    checkmark.style.display = "block"
+    anim()
+    setTimeout(function () {
+        submittedText.style.opacity = 1
+    }, 1500)
+}
+
+var submit = document.getElementById("submit")
 submit.addEventListener("mouseenter", function () {
     submit.style.scale = "120%"
 })
@@ -11,6 +34,6 @@ submit.addEventListener("mousedown", function () {
 })
 submit.addEventListener("mouseup", function () {
     submit.style.scale = "120%"
-    submitAnim()
-    submitFunc()
+    SubmitAnim()
+    SubmitFunc()
 })
